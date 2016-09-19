dd1c9af2195ff118fe01d4bc5100fd7746836392
Jens Vonderheide <jens@vdheide.de>
2016-09-19 15:53:14 +0200

    Renamed Separated{Storage|RGB}Canvas to Planar... to better match the DICOM vocabulary

--- spec/planar-rgb-canvas-spec.js
+++ spec/planar-rgb-canvas-spec.js
@@ -1,12 +1,12 @@
 /* eslint-env node, jasmine */
 "use strict";
 
-const SeparatedRGBCanvas = require("../lib/canvas/SeparatedRGBCanvas");
+const PlanarRGBCanvas = require("../lib/canvas/PlanarRGBCanvas");
 const bitmapManipulation = require("..");
 
 describe("Creation", function () {
     it("creates a Buffer sized to bytes per pixel * columns * rows * 3", function () {
-        let image = new SeparatedRGBCanvas(5, 10, 2);
+        let image = new PlanarRGBCanvas(5, 10, 2);
 
         expect(image.data).toEqual(jasmine.any(Buffer));
         expect(image.data.length).toBe(5 * 10 * 2 * 3);
@@ -16,7 +16,7 @@ describe("Creation", function () {
 describe("setPixel", function () {
     describe("with 1 byte per pixel", function () {
         it("sets three bytes in the data array", function () {
-            let canvas = new SeparatedRGBCanvas(3, 3, 1);
+            let canvas = new PlanarRGBCanvas(3, 3, 1);
             canvas.setPixel(0, 0, [10, 20, 30]);
             expect(canvas.data[0]).toBe(10);
             expect(canvas.data[9]).toBe(20);
@@ -31,7 +31,7 @@ describe("setPixel", function () {
 
     describe("with 2 bytes per pixel", function () {
         it("sets the corresponding bytes in the data array", function () {
-            let canvas = new SeparatedRGBCanvas(3, 3, 2);
+            let canvas = new PlanarRGBCanvas(3, 3, 2);
             canvas.setPixel(0, 0, [1000, 2000, 3000]);
             expect(canvas.data[0]).toBe(3);
             expect(canvas.data[1]).toBe(232);
@@ -50,7 +50,7 @@ describe("setPixel", function () {
         });
 
         it("writes the pixel data in low-high order when set to little endian", function () {
-            let canvas = new SeparatedRGBCanvas(3, 3, 2, bitmapManipulation.Endianness.LITTLE);
+            let canvas = new PlanarRGBCanvas(3, 3, 2, bitmapManipulation.Endianness.LITTLE);
             canvas.setPixel(1, 2, [1000, 2000, 3000]);
             expect(canvas.data[14]).toBe(232);
             expect(canvas.data[15]).toBe(3);
@@ -64,7 +64,7 @@ describe("setPixel", function () {
 
 describe("getPixel", function () {
     it("builds an RGB array from the correct bytes", function () {
-        let canvas = new SeparatedRGBCanvas(3, 3, 2);
+        let canvas = new PlanarRGBCanvas(3, 3, 2);
         canvas.data[14] = 3;
         canvas.data[15] = 232;
         canvas.data[32] = 7;
@@ -75,7 +75,7 @@ describe("getPixel", function () {
     });
 
     it("builds an RGB array from the correct bytes in little endian", function () {
-        let canvas = new SeparatedRGBCanvas(3, 3, 2, bitmapManipulation.Endianness.LITTLE);
+        let canvas = new PlanarRGBCanvas(3, 3, 2, bitmapManipulation.Endianness.LITTLE);
         canvas.data[14] = 232;
         canvas.data[15] = 3;
         canvas.data[32] = 208;
